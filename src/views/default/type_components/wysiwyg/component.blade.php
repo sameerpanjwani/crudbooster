062566aaa484d211085d4b33b39db33a2290db0d
Mondovo <sameer@mondovo.com>
2018-12-02 12:50:15 +0530

    Update component.blade.php

--- src/views/default/type_components/wysiwyg/component.blade.php
+++ src/views/default/type_components/wysiwyg/component.blade.php
@@ -36,7 +36,7 @@
     <label class='control-label col-sm-2'>{{$form['label']}}</label>
 
     <div class="{{$col_width?:'col-sm-10'}}">
-        <textarea id='textarea_{{$name}}' id="{{$name}}" {{$required}} {{$readonly}} {{$disabled}} name="{{$form['name']}}" class='form-control'
+        <textarea id='textarea_{{$name}}' id="{{$name}}" data-validation="{{$required}}" {{$readonly}} {{$disabled}} name="{{$form['name']}}" class='form-control'
                   rows='5'>{{ $value }}</textarea>
         <div class="text-danger">{{ $errors->first($name) }}</div>
         <p class='help-block'>{{ @$form['help'] }}</p>
